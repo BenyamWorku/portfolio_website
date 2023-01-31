933a520a946e9eb9e89c35abff5cd62d8c08b0f0
Benyam <benworku@gmail.com>
2023-01-31 00:34:01 +0000

    hero-class major work done

---
+++ portfolio/app.js
@@ -0,0 +1,12 @@
+const typewriter = document.querySelector(".typewriter");
+  const caret = typewriter.style.borderRight;
+  const text = typewriter.innerHTML;
+  const time = text.length * 15; // adjust time based on text length
+  const typingSound = document.querySelector("#typing-sound");
+
+//   typewriter.style.animation = `typing ${time}ms steps(40, end), blink-caret 0.75s step-end forwards`;
+
+  typewriter.addEventListener("animationstart", () => {
+    typingSound.currentTime = 0;
+    typingSound.play();
+  });
