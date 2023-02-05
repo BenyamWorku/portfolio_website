5592fb06a2d9be2b75c47bac68f5018f8254e37f
Benyam <benworku@gmail.com>
2023-02-05 21:50:32 +0000

    button is working now

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ const typewriter = document.querySelector(".typewriter");
     typingSound.currentTime = 0;
     typingSound.play();
   });
+// button is clicked and takes us to the about me page
 
+  document.getElementById("about-me-button").addEventListener("click", function() {
+    location.hash = "#about-me";
+  });
+
+// toggle the hamburger icon depending on the size of the viewport
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
@@ -26,6 +32,7 @@ document.getElementById("email-link").addEventListener("click",function(event)
   window.open("options.html","choose Email Provider","width=400,height=500");
 
 })
+
 
 
 
